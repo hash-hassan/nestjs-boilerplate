8ed00605cecec1a99bec8982ed8fc6794f06e7ad
hamzachangecx <hamza.shakeel@changecx.com>
2023-04-06 15:59:07 +0500

    Initail Commit

---
+++ src/configs/database.config.ts
@@ -0,0 +1,16 @@
+import { registerAs } from '@nestjs/config';
+
+export default registerAs(
+    'database',
+    (): Record<string, any> => ({
+        host: process.env?.DATABASE_HOST ?? 'mongodb://localhost:27017',
+        name: process.env?.DATABASE_NAME ?? 'ack',
+        user: process.env?.DATABASE_USER,
+        password: process?.env.DATABASE_PASSWORD,
+        debug: process.env.DATABASE_DEBUG === 'true',
+        options: {
+            retryWrites: true,
+            w: 'majority',
+          },
+    })
+);
